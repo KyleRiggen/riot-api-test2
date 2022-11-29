61fdf3760037ab8e4665a6cd63ecababf4f13842
KyleRiggen <kyle.riggenbach@gmail.com>
2022-11-29 00:42:33 -0700

    trying to get teemojs to work

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const https = require('https');
 
+const TeemoJS = require('teemojs');
+let api = TeemoJS(process.env.RIOT_GAMES_API_KEY);
+
 const app = express();
 console.log("token", process.env.WEATHER_API_KEY);
 
@@ -13,12 +16,17 @@ app.get("/", function(req, res) {
         response.on("data", function(data) {
             const weatherData = JSON.parse(data);
             const population = weatherData.city.population;
-            console.log(population);
+            res.send("population is " + population);
         });
-    })
-
-    res.send("server is up and running")
+    });
 });
+
+api.get('champion.getChampion', 143)
+  .then(data => console.log(data));
+
+// Get C9 Sneaky's games on Ezreal and Kalista for a particular season.
+// api.get('na1', 'match.getMatchlist', 78247, { champion: [81, 429], season: 8 })
+  //.then(data => console.log(data));
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000")
